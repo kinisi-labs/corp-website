42a8c87f71fdb6ecc6fd5a0363f18c1605280751
manu chatterjee <deftio@deftio.com>
2021-02-12 17:07:59 -0800

    updated

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+header("Location: ./bs/Selecao/index.html")
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -201,7 +204,7 @@
 <!-- FOOTER -->
 <footer class="container">
   <p class="float-right"><a href="#">Back to top</a></p>
-  <p>&copy; 2020 Kinisi Inc.  &middot; All Rights Reserved.<!-- a href="#">Privacy</a> &middot; <a href="#">Terms</a> --></p>
+  <p>&copy; 2021 Kinisi Inc.  &middot; All Rights Reserved.<!-- a href="#">Privacy</a> &middot; <a href="#">Terms</a> --></p>
 </footer>
 </main>
 
